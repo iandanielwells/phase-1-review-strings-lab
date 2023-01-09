1e4974e01e1c01dde256587be34dfa427445b96d
iandanielwells <iandanielwells@gmail.com>
2023-01-09 14:45:48 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Ian Wells';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser +'!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!';
+
+
+
+
+
+
+
